e14c57f083580bffc79a1838139a3e40783203ad
pramodbn <begurnagaraj.p@northeastern.edu>
2023-08-06 21:26:55 -0500

    Frontend

---
+++ Frontend/script.js
@@ -0,0 +1,39 @@
+function submitForm() {
+    event.preventDefault();
+
+    // Get form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const password = document.getElementById('password').value;
+
+    // Create request object
+    const xhr = new XMLHttpRequest();
+
+    // Set up request
+    xhr.open('POST', 'https://6k3bebla27.execute-api.us-west-2.amazonaws.com/Prod/register', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    // Set up response handler
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                alert('Registration successful!');
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('phone').value = '';
+                document.getElementById('password').value = '';
+            } else {
+                alert('Registration failed: ' + xhr.responseText);
+            }
+        }
+    };
+
+    // Send request
+    xhr.send(JSON.stringify({
+        name: name,
+        email: email,
+        phone: phone,
+        password: password
+    }));
+}
